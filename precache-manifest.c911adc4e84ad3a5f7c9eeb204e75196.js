0ab51d05dcb036f964e829f16e4108ef29cb0980
Anton <toxasava@icloud.com>
2019-07-19 20:17:52 -0400

    Updates

---
+++ precache-manifest.c911adc4e84ad3a5f7c9eeb204e75196.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46fbbab58f2b91ba344eb04893c1d0aa",
+    "url": "/mason/index.html"
+  },
+  {
+    "revision": "5decdc38e807294b200c",
+    "url": "/mason/static/css/main.ede2c40f.chunk.css"
+  },
+  {
+    "revision": "7ec99751e057e71118f8",
+    "url": "/mason/static/js/2.ff82d4c3.chunk.js"
+  },
+  {
+    "revision": "5decdc38e807294b200c",
+    "url": "/mason/static/js/main.232259fe.chunk.js"
+  },
+  {
+    "revision": "29e26906c49dcefbec29",
+    "url": "/mason/static/js/runtime~main.fe710cf6.js"
+  }
+]);
